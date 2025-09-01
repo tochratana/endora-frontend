5e0da1e1047a91defb57b6d4f1be4b100ef96596
RML-Cambo <ltomusic69@gmail.com>
2025-09-01 18:41:44 +0700

    Fix: sth

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,45 +1,10 @@
-"use client";
-
-import React, { Suspense } from "react";
-import dynamic from "next/dynamic";
-import HeroSection from "../../components/home/HeroSection";
-
-const FeaturesSection = dynamic(
-  () => import("../../components/home/FeaturesSection"),
-  {
-    loading: () => <div>Loading...</div>,
-    ssr: false,
-  }
-);
-
-const OurServices = dynamic(() => import("../../components/home/OurServices"), {
-  loading: () => <div>Loading...</div>,
-  ssr: false,
-});
-
-const FrameworkSection = dynamic(
-  () => import("../../components/home/FramworkSpp"),
-  {
-    loading: () => <div>Loading...</div>,
-    ssr: false,
-  }
-);
-
-const APIDesign = dynamic(() => import("../../components/home/APIDesign"), {
-  loading: () => <div>Loading...</div>,
-  ssr: false,
-});
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <HeroSection />
-        <FeaturesSection />
-        <OurServices />
-        <FrameworkSection />
-        <APIDesign />
-      </Suspense>
-    </div>
+    <main>
+      <h1>Home (test)</h1>
+      <p>This is a minimal page used to isolate build issues.</p>
+    </main>
   );
 }
