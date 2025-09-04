f0e193d9508c410fa536c599a731ae8ba10cc8f1
K-taiyi <korm.taiyi289@gmail.com>
2025-09-04 10:01:47 +0700

    change background image

--- src/components/about/JoinTeam.tsx
+++ src/components/about/JoinTeam.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function JoinTeam() {
   return (
     <section className="w-full py-10 px-2 bg-white">
-      <div className="max-w-6xl m-auto text-center font-semibold min-h-[400px]">
+      <div className="max-w-6xl m-auto text-center font-semibold">
         <h1 className="text-(--color-secondary) md:text-5xl text-4xl mb-7">Joint Our Team</h1>
-        <p className="text-black md:w-[50%] w-full m-auto mb-5">
+        <p className="text-black md:w-[60%] w-full m-auto mb-5">
           We are delighted to be a part of your project journey by delivering
           fast and effective tools that accelerate your progress. Whether you’re
           starting small or scaling up, we’re here to ensure your development
--- src/components/projectoverview/ProjectOverview.tsx
+++ src/components/projectoverview/ProjectOverview.tsx
@@ -2,31 +2,31 @@ import { Archive, Database, User, ZapIcon } from "lucide-react";
 
 export default function ProjectOverview() {
   return (
-   <section className="w-full py-10 bg-gray-100">
-      <div className="max-w-6xl m-auto flex md:flex-col flex-row text-black">
+   <section className="w-full py-10 dark:bg-slate-900 bg-white">
+      <div className="max-w-6xl m-auto flex md:flex-col flex-row text-black px-2">
         <div className="grid grid-cols-12 w-full mb-10">
           <div className="col-span-6">
-            <h1 className="text-2xl font-semibold mb-3 text-black">Daron-Beauty-Store</h1>
-            <p className="text-gray-800">We bring convenience to your fingertips with a wide range of products tailored to your lifestyle.</p>
+            <h1 className="text-2xl font-semibold mb-3 text-gray-800 dark:text-white">Daron-Beauty-Store</h1>
+            <p className="text-gray-800 dark:text-gray-400">We bring convenience to your fingertips with a wide range of products tailored to your lifestyle.</p>
           </div>
-          <div className="text-black col-span-6 justify-self-end">
-            <h1 className="font-semibold text-lg">Total</h1>
-            <p>00</p>
+          <div className="col-span-6 justify-self-end ">
+            <h1 className="font-semibold text-lg text-gray-800 dark:text-white">Total</h1>
+            <p className="text-gray-800 dark:text-gray-300">00</p>
           </div>
         </div>
-        <div className="grid grid-cols-8 h-70 w-full gap-3">
+        <div className="grid grid-cols-8 h-70 w-full gap-4">
           {/*  */}
           <div className="col-span-2 p-3 border rounded border-gray-300">
             <div className="h-[50%]">
               <div className="flex gap-4 mb-3">
                 <Database className="text-[var(--color-secondary)]"/>
-                <h1 className="font-semibold">Database</h1>
+                <h1 className="font-semibold text-gray-800 dark:text-gray-300">Schema</h1>
               </div>
-              <h3 className="text-sm font-semibold">REST Request</h3>
-              <p>0</p>
+              <h3 className="text-sm font-semibold text-gray-800 dark:text-gray-300">REST Request</h3>
+              <p className="text-gray-800 dark:text-gray-300">0</p>
             </div>
             <div className="h-[50%] grid place-content-center border border-gray-300 text-center">
-              <p>No data to show</p>
+              <p className="text-gray-800 dark:text-gray-300">No data to show</p>
             </div>
           </div>
           {/*  */}
