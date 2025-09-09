fc01bba7d8cd16d56e0591a27dda03f6343ce380
K-taiyi <korm.taiyi289@gmail.com>
2025-09-09 12:17:21 +0700

    chage ui

--- src/components/about/HeroSection.tsx
+++ src/components/about/HeroSection.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <section className="max-w-6xl m-auto flex flex-col item-center text-center justify-center py-15 gap-5">
-      <h1 className="md:text-5xl text-4xl font-semibold text-black dark:text-white">
+      <h1 className="md:text-5xl text-4xl font-semibold text-gray-800 dark:text-white">
         Welcome to <span className="text-(--color-secondary)">Endora</span>
       </h1>
-      <p className="md:w-[60%] w-[80%] m-auto md:text-lg font-bold text-black dark:text-white">
+      <p className="md:w-[60%] w-[80%] m-auto md:text-lg font-bold text-gray-800 dark:text-white">
         Endora is a lightweight Backend-as-a-Service(Baas) platform that
         simplifies backend development by generating REST APIs from schemas or
         data sources.
